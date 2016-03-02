451a4e73963d3f3fa2111751afe43c196937bc6b
unknown <rasmusvhansen@gmail.com>
2016-03-02 21:46:30 +0100

    Added clang-format

--- concatFunctions/index.js
+++ concatFunctions/index.js
@@ -1,11 +1,18 @@
 var fs = require('fs');
 function concatFiles(json, output) {
-    var files = JSON.parse(fs.readFileSync(json.trim(), 'utf8'));
-    var concatted = files.reduce(function (prev, next) { return prev + fs.readFileSync(next, 'utf8') + '\n'; }, '').trim();
-    
-    output ? fs.writeFileSync(output, concatted) : process.stdout.write(concatted);
+  var files = JSON.parse(fs.readFileSync(json.trim(), 'utf8'));
+  var concatted =
+      files.reduce(function(prev, next) {
+             return prev + fs.readFileSync(next, 'utf8') + '\n';
+           }, '').trim();
+
+  if (output) {
+    fs.writeFileSync(output, concatted)
+  } else {
+    process.stdout.write(concatted);
+  }
 }
 
 module.exports = {
-    concatFiles: concatFiles    
+  concatFiles : concatFiles
 };
--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 #!/usr/bin/env node
-var exampleTxt = 'This will concatenate all the files in the files.json and pipe them to the file specified by the -o argument';
+var exampleTxt =
+    'This will concatenate all the files in the files.json and pipe them to the file specified by the -o argument';
 var stdoutTxt = 'This will concatenate all the files in the files.json and pipe them to stdout';
 var argv = require('yargs')
-    .option('j', {
-        alias: 'json',
-        demand: true,
-        describe: 'json file containing array of files to concatenate',
-        type: 'string'
-    })
-    .option('o', {
-        alias: 'output',        
-        describe: 'the optional resulting file of the concatenation',
-        type: 'string'
-    })
-    .usage('$0 concat-from-json -j string [-o string]')    
-    .example('concat-from-json -j files.json -o output.js', exampleTxt)
-    .example('concat-from-json -j files.json > output.js', stdoutTxt)
-    .help('help')
-    .argv;
-    
+               .option('j', {
+                 alias : 'json',
+                 demand : true,
+                 describe : 'json file containing array of files to concatenate',
+                 type : 'string'
+               })
+               .option('o', {
+                 alias : 'output',
+                 describe : 'the optional resulting file of the concatenation',
+                 type : 'string'
+               })
+               .usage('$0 concat-from-json -j string [-o string]')
+               .example('concat-from-json -j files.json -o output.js', exampleTxt)
+               .example('concat-from-json -j files.json > output.js', stdoutTxt)
+               .help('help')
+               .argv;
+
 var concatFunctions = require('./concatFunctions');
 
 concatFunctions.concatFiles(argv.j, argv.o);
