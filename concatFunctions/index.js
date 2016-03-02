3ea17bfd3158efeaae12307b5505514618e8f16b
unknown <rasmusvhansen@gmail.com>
2016-03-02 20:30:04 +0100

    Output to file

--- concatFunctions/index.js
+++ concatFunctions/index.js
@@ -3,7 +3,7 @@ function concatFiles(json, output) {
     var files = JSON.parse(fs.readFileSync(json.trim(), 'utf8'));
     var concatted = files.reduce(function (prev, next) { return prev + fs.readFileSync(next, 'utf8') + '\n'; }, '');
     
-    output ? fs.writeFileSync(concatted) : process.stdout.write(concatted);
+    output ? fs.writeFileSync(output, concatted) : process.stdout.write(concatted);
 }
 
 module.exports = {
