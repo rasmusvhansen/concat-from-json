685cabc29bc081bfef1c73885edbe14b73382453
Rasmus Vestergaard Hansen <rasmusvhansen@gmail.com>
2016-04-04 12:37:06 +0000

    added glob support

--- concatFunctions/index.js
+++ concatFunctions/index.js
@@ -1,10 +1,20 @@
 var fs = require('fs');
+var glob = require('glob');
 function concatFiles(json, output) {
-  var files = JSON.parse(fs.readFileSync(json.trim(), 'utf8'));
+  var files = flatmap(JSON.parse(fs.readFileSync(json.trim(), 'utf8'))
+    .map(function (pattern) {
+      console.log(pattern);
+      var files = glob.sync(pattern);
+      console.log(files);
+      return files;
+    }), function (file) {
+    return file;
+  });
+
   var concatted =
-      files.reduce(function(prev, next) {
-             return prev + fs.readFileSync(next, 'utf8') + '\n';
-           }, '').trim();
+    files.reduce(function (prev, next) {
+      return prev + fs.readFileSync(next, 'utf8') + '\n';
+    }, '').trim();
 
   if (output) {
     fs.writeFileSync(output, concatted)
@@ -14,5 +24,19 @@ function concatFiles(json, output) {
 }
 
 module.exports = {
-  concatFiles : concatFiles
+  concatFiles: concatFiles
+};
+
+function flatmap(arr, iter, context) {
+  var results = [];
+  if (!Array.isArray(arr)) return results;
+  arr.forEach(function (value, index, list) {
+    var res = iter.call(context, value, index, list);
+    if (Array.isArray(res)) {
+      results.push.apply(results, res);
+    } else if (res != null) {
+      results.push(res);
+    }
+  });
+  return results;
 };
