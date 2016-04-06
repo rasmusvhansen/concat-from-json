c5226d3150d0bdd0e697f0af7fa1045dc68f7e5e
Rasmus Vestergaard Hansen <rasmusvhansen@gmail.com>
2016-04-06 08:15:06 +0000

    fixed sourcemaps and multiglob

--- concatFunctions/index.js
+++ concatFunctions/index.js
@@ -4,44 +4,28 @@ var Concat = require('concat-with-sourcemaps')
 
 function concatFiles(json, output) {
   var concat = new Concat(true, output, '\n');
-  var files = flatmap(JSON.parse(fs.readFileSync(json.trim(), 'utf8'))
-    .map(function(pattern) {
-      var files = glob.sync(pattern);
-      return files;
-    }),
-    function(file) {
-      return file;
+  var globs = JSON.parse(fs.readFileSync(json.trim(), 'utf8'));
+  console.log(globs);
+  glob(globs, {strict: true}, function(err, files) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    files.forEach(function(f) {
+      concat.add(f, fs.readFileSync(f, 'utf8'));
     });
 
-  files.forEach(function(f) {
-    concat.add(f, fs.readFileSync(f, 'utf8'));
+    if (output) {
+      var mapFile = output + '.map';
+      fs.writeFile(output, concat.content + '\n//# sourceMappingURL=/' + mapFile.replace('webapp/', ''), 'utf-8');
+      fs.writeFile(output + '.map', concat.sourceMap, 'utf-8');
+    }
+    else {
+      process.stdout.write(concat.content, 'utf-8');
+    }
   });
- 
-  if (output) {
-    var mapFile = output + '.map';
-    fs.writeFile(output, concat.content + '\n//# sourceMappingURL=/'+mapFile.replace('webapp/', ''), 'utf-8');
-    fs.writeFile(output + '.map', concat.sourceMap, 'utf-8');
-  }
-  else {
-    process.stdout.write(concat.content, 'utf-8');
-  }
 }
 
 module.exports = {
   concatFiles: concatFiles
-};
-
-function flatmap(arr, iter, context) {
-  var results = [];
-  if (!Array.isArray(arr)) return results;
-  arr.forEach(function(value, index, list) {
-    var res = iter.call(context, value, index, list);
-    if (Array.isArray(res)) {
-      results.push.apply(results, res);
-    }
-    else if (res != null) {
-      results.push(res);
-    }
-  });
-  return results;
 };
